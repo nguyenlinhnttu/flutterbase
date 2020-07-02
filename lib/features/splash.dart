e74ed128fbcbfcb729bc93d8c01fdf3c8f47acf9
NguyenLinh <nguyenlinh.nttu@gmail.com>
2020-07-02 22:19:03 +0700

    - Update indicator.

--- lib/features/splash.dart
+++ lib/features/splash.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_base_source/providers/meta_provider.dart';
+import 'package:flutter_base_source/widgets/common_view.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
@@ -27,69 +27,66 @@ class _SplashState extends State<Splash> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 1), (){
+    Timer(Duration(milliseconds: 500), () {
       Provider.of<MetaProvider>(context, listen: false).getMetaData();
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<MetaProvider>(
-        builder: (BuildContext context, MetaProvider metaProvider, Widget child) {
-          return Scaffold(
-            body: WillPopScope(
-              onWillPop: () => Future.value(false),
-              child: Stack(fit: StackFit.expand, children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(40),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Hello Splash Screen',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      Center(
-                        child: Image.asset(
-                          "assets/icons/ic_splash.png",
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: SafeArea(
-                    child: InkWell(
-                      onTap: () {
-                        nextPage();
-                      },
-                      child: Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          margin: EdgeInsets.only(right: 40, bottom: 20),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(
-                                color: Theme
-                                    .of(context)
-                                    .accentColor, width: 1),
-                          ),
-                          child: Text(
-                            'Start ...!',
-                          )),
+    return Consumer<MetaProvider>(builder:
+        (BuildContext context, MetaProvider metaProvider, Widget child) {
+      return Scaffold(
+        body: WillPopScope(
+          onWillPop: () => Future.value(false),
+          child: Stack(fit: StackFit.expand, children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Hello Splash Screen',
+                    style: TextStyle(
+                      fontSize: 20.0,
                     ),
                   ),
-                ),
-                metaProvider.isLoading()? Center(child: CircularProgressIndicator()) :Container(),
-              ]),
+                  SizedBox(height: 50),
+                  Center(
+                    child: Image.asset(
+                      "assets/icons/ic_splash.png",
+                    ),
+                  ),
+                ],
+              ),
             ),
-          );
-        });
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SafeArea(
+                child: InkWell(
+                  onTap: () {
+                    nextPage();
+                  },
+                  child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      margin: EdgeInsets.only(right: 40, bottom: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                            color: Theme.of(context).accentColor, width: 1),
+                      ),
+                      child: Text(
+                        'Start ...!',
+                      )),
+                ),
+              ),
+            ),
+            CommonView.loadingView(metaProvider.isLoading())
+          ]),
+        ),
+      );
+    });
   }
 }
